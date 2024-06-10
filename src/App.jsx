c05462af1b0a17ce7714f92231a05c8ab3a95eec
huycuongngo <cuong.nh@softenmind.com>
2024-06-10 19:48:55 +0700

    update: page not found

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/Layout'
 import HomePage from './pages/movie/HomePage'
 import PrivateRoute from './routes/PrivateRoute'
+import MovieDetailPage from './pages/movie/MovieDetailPage'
+import DashboardAdmin from './pages/user/DashboardAdmin'
+import Login from './pages/user/Login'
+import Register from './pages/user/Register'
 
 function App() {
 
@@ -11,7 +15,14 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<PrivateRoute element={<HomePage />} />} />
+          <Route path='/movies/:id' element={<PrivateRoute element={<MovieDetailPage />} />} />
+          <Route path='/admin' element={<PrivateRoute element={<DashboardAdmin />} />} />
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Route>
+
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   )
